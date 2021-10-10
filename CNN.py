a5375c5cd10e59969e4eab1396d45571db05b7a7
Salach-Malejkum <mateuszsalach00@gmail.com>
2021-10-10 15:40:05 +0200

    Add implementation of train and evaluate functions

--- CNN.py
+++ CNN.py
@@ -7,15 +7,16 @@ class ConvolutionalNeuralNetwork:
     def __init__(self):
         self.network = Sequential()
         self.network.add(Flatten(input_shape=[1000, 400]))
-        self.network.add(LeakyReLU(alpha=0.1))
-        self.network.add(Dense(4, activation="softmax"))  # output layer
+        self.network.add(LeakyReLU(alpha=0.1))  # is it enough?
+        self.network.add(Dense(4, activation="softmax"))
 
         self.network.compile(optimizer='adam',
                              loss='categorical_crossentropy',
                              metrics=['accuracy'])
 
-    def train(self):
-        pass
+    def train(self, X_train, y_train, X_val, y_val, epochs):
+        history = self.network.fit(X_train / 255, y_train, epochs=epochs, validation_data=(X_val / 255, y_val))
+        return history.history
 
-    def classify(self):
-        pass
+    def evaluate(self, X_test, y_test):
+        return self.model.evaluate(X_test / 255, y_test)
