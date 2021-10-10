2dcb0ef8f3e2bc4d5c10bfa418b0b2a20ece063e
Salach-Malejkum <mateuszsalach00@gmail.com>
2021-10-10 15:05:08 +0200

    Add input size, add compile parameters

--- CNN.py
+++ CNN.py
@@ -1,13 +1,18 @@
 import keras
 from keras.models import Sequential, Input, Model
-from keras.layers import Softmax, ReLU, LeakyReLU, Dense
+from keras.layers import Softmax, ReLU, LeakyReLU, Dense, Flatten
 
 
 class ConvolutionalNeuralNetwork:
     def __init__(self):
         self.network = Sequential()
+        self.network.add(Flatten(input_shape=[1000, 400]))
         self.network.add(LeakyReLU(alpha=0.1))
         self.network.add(Dense(4, activation="softmax"))  # output layer
+
+        self.network.compile(optimizer='adam',
+                             loss='categorical_crossentropy',
+                             metrics=['accuracy'])
 
     def train(self):
         pass
