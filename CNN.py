ad1bf36c518e79915abddf4358b5fe1af0e8582d
Salach-Malejkum <mateuszsalach00@gmail.com>
2021-10-17 13:18:26 +0200

    Add changes to CNN according to input data

--- CNN.py
+++ CNN.py
@@ -1,22 +1,28 @@
 import keras
 from keras.models import Sequential, Input, Model
-from keras.layers import Softmax, ReLU, LeakyReLU, Dense, Flatten
+from keras.layers import Softmax, ReLU, LeakyReLU, Dense, Flatten, Rescaling
 
 
 class ConvolutionalNeuralNetwork:
-    def __init__(self):
-        self.network = Sequential()
+    def __init__(self, layers, neurons):
+        self.network = Sequential([
+            Rescaling(1./255),
+        ])
         self.network.add(Flatten(input_shape=[1000, 400]))
-        self.network.add(LeakyReLU(alpha=0.1))  # is it enough?
-        self.network.add(Dense(4, activation="softmax"))
+
+        for num in range(layers):
+            self.network.add(Dense(neurons, activation=LeakyReLU(alpha=0.1)))
+
+        self.network.add(Dense(5, activation="softmax"))
+
 
         self.network.compile(optimizer='adam',
                              loss='categorical_crossentropy',
                              metrics=['accuracy'])
 
-    def train(self, X_train, y_train, X_val, y_val, epochs):
-        history = self.network.fit(X_train / 255, y_train, epochs=epochs, validation_data=(X_val / 255, y_val))
+    def train(self, train, val, epochs):
+        history = self.network.fit(train, epochs=epochs, validation_data=val)
         return history.history
 
-    def evaluate(self, X_test, y_test):
-        return self.network.evaluate(X_test / 255, y_test)
+    def evaluate(self, test):
+        return self.network.evaluate(test)
