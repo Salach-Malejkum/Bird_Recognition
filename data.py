c8fe2d4d910871651380052369a70f11661e45e0
Salach-Malejkum <mateuszsalach00@gmail.com>
2021-10-17 13:17:38 +0200

    Add two way to read data for CNN

--- data.py
+++ data.py
@@ -1,9 +1,23 @@
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+import PIL.Image
+from tensorflow.keras.utils import image_dataset_from_directory
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import pathlib
 
 
 def get_training_data():
-    data_set = ImageDataGenerator()
-    train_it = data_set.flow_from_directory('data/training/', class_mode='categorical', batch_size=3)
-    val_it = data_set.flow_from_directory('data/validation/', class_mode='categorical', batch_size=2)
-    test_it = data_set.flow_from_directory('data/test/', class_mode='categorical', batch_size=2)
-    return (train_it, val_it, test_it)
+    # train_dir = pathlib.Path('./data/training')
+    # val_dir = pathlib.Path('./data/validation')
+    # test_dir = pathlib.Path('./data/test')
+    #
+    # train_ds = image_dataset_from_directory(train_dir, image_size=(100, 40), batch_size=32)
+    # val_ds = image_dataset_from_directory(val_dir, image_size=(100, 40), batch_size=32)
+    # test_ds = image_dataset_from_directory(test_dir, image_size=(100, 40), batch_size=32)
+    #
+    # return train_ds, val_ds, test_ds
+    data_set = ImageDataGenerator(rescale=1/255.,
+                                  )
+
+    train_gen = data_set.flow_from_directory('data/training/', target_size=(1000, 400), class_mode='categorical', batch_size=32)
+    val_gen = data_set.flow_from_directory('data/validation/', target_size=(1000, 400), class_mode='categorical', batch_size=32)
+    test_gen = data_set.flow_from_directory('data/test/', target_size=(1000, 400), class_mode='categorical', batch_size=32)
+    return (train_gen, val_gen, test_gen)
